e075e73104b543b69995c112d52f13ee3aad4f76
Nguyen Hoang Dieu <dnguyen@karrostech.com>
2020-06-01 07:43:03 +0700

    add eureka server and config eureka client for all services

--- eurekaserver/src/main/java/com/icommerce/eurekaserver/ServerApplication.java
+++ eurekaserver/src/main/java/com/icommerce/eurekaserver/ServerApplication.java
@@ -1,10 +1,10 @@
-package com.icommerce.productservice;
+package com.icommerce.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
 public class ServerApplication {
 
