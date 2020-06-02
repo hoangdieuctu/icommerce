819955935e3784bfb9ad19bec27a05f4d27c15ba
Nguyen Hoang Dieu <dnguyen@karrostech.com>
2020-06-03 06:56:10 +0700

    shopping cart service: move config to config repo

--- productservice/src/test/java/com/icommerce/productservice/service/ProductServiceSpringTest.java
+++ productservice/src/test/java/com/icommerce/productservice/service/ProductServiceSpringTest.java
@@ -13,14 +13,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@ActiveProfiles(value = "test")
 public class ProductServiceSpringTest {
 
     @Autowired
