026698f0ed494f9a97f446d633ffe2bb20e4e23e
Nguyen Hoang Dieu <dnguyen@karrostech.com>
2020-06-03 08:00:43 +0700

    add api gateway and change properties file to yml file

--- apigateway/src/main/java/com/icommerce/apigateway/ServerApplication.java
+++ apigateway/src/main/java/com/icommerce/apigateway/ServerApplication.java
@@ -1,24 +1,12 @@
-package com.icommerce.productservice;
+package com.icommerce.apigateway;
 
-import com.icommerce.productservice.generator.ProductGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import javax.annotation.PostConstruct;
-
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServerApplication {
-
-    @Autowired
-    private ProductGenerator productGenerator;
-
-    @PostConstruct
-    public void generateData() {
-        productGenerator.generateProducts();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
--- productservice/src/main/java/com/icommerce/productservice/generator/ProductGenerator.java
+++ productservice/src/main/java/com/icommerce/productservice/generator/ProductGenerator.java
@@ -5,13 +5,14 @@ import com.icommerce.productservice.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.Date;
+import java.util.List;
 
 @Component
+@ConfigurationProperties(prefix = "generator")
 public class ProductGenerator {
 
     private Logger logger = LoggerFactory.getLogger(ProductGenerator.class);
@@ -19,18 +20,25 @@ public class ProductGenerator {
     @Autowired
     private ProductRepository productRepository;
 
-    @Value("${generator.products:#{null}}")
-    private String products;
+    private List<String> products;
 
-    @PostConstruct
+    // for binding configuration
+    public List<String> getProducts() {
+        return products;
+    }
+
+    // for binding configuration
+    public void setProducts(List<String> products) {
+        this.products = products;
+    }
+
     public void generateProducts() {
         if (products == null || products.isEmpty()) {
             return;
         }
 
         logger.info("Generate products");
-        String[] items = products.split(";");
-        for (String item : items) {
+        for (String item : products) {
             String[] parts = item.split(",");
 
             Product product = new Product();
